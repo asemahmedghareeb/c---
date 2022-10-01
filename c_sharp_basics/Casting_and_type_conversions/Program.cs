2882aa93655fb23924fe7bc374d822e7149913ee
asemahmedghareeb <asemelbadahy123@gmail.com>
2022-10-01 08:03:39 +0200

    new

--- c_sharp_basics/Casting_and_type_conversions/Program.cs
+++ c_sharp_basics/Casting_and_type_conversions/Program.cs
@@ -5,8 +5,33 @@ namespace casting
     {
         static void Main(string[] args)
         {
-            for(int i=0; i<5; i++)
-            Console.WriteLine("hello world");
+            Int16 s;// == short
+            int  x;//== int32 x;
+            Int64 l;//long
+            short y;
+            ushort unsigned_short;//==uint16
+            ulong d;//== uint64
+            
+            var num=10;
+            string str=num.ToString();
+            int numberint=10;
+            long numberlong=10;
+            numberlong=numberint;
+            //dotnet doesn't convert long to int automatically
+
+            //numberint=numberlong;
+
+
+            //explicit casting
+            if(numberlong<=int.MaxValue){
+                numberint=(int)numberlong;
+            }
+            
+            double t=123.123;
+            int i=(int)t;
+            Console.Write(i);
+            
+            
             Console.ReadKey();
         }
     }
