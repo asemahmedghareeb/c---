cb4b2885b9660dd2299981fa9572acd5d63ec8dd
asemahmedghareeb <asemelbadahy123@gmail.com>
2022-10-03 16:16:31 +0200

    new

--- c_sharp_basics/Methods/Program.cs
+++ c_sharp_basics/Methods/Program.cs
@@ -5,7 +5,23 @@ namespace app
     {
         static void Main()
         {
+            demo d = new demo();
+            var val =d.DoSomeThing2();
+            Console.WriteLine(val); 
+        }
+    }
+    public class demo
+    {
+        //method
+        public  void DoSomeThing()
+        {
+            Console.WriteLine("do some thing");
             
+        }
+        public int DoSomeThing2()
+        {
+
+            return 10;
         }
     }
 }
