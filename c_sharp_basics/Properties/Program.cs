b5ec045f7f0860559755af6d5fd46f622d4d0aaa
asemahmedghareeb <asemelbadahy123@gmail.com>
2022-10-11 20:46:31 +0200

    new

--- c_sharp_basics/Properties/Program.cs
+++ c_sharp_basics/Properties/Program.cs
@@ -6,24 +6,48 @@ namespace app
         static void Main(string[] args)
         {
 
-        Doller doller = new Doller(100);
-            
+            Doller doller = new Doller(100);
+            doller.SetAmount(1.99m);
+            Console.WriteLine(doller.Amount);
+            Console.WriteLine(doller.Is_Zero);
 
         }
     }
 
     public class Doller
     {
-        private decimal _amount;
+        private  decimal _amount;
+        //properity
+        public decimal Amount
+        {
+            get 
+            { 
+                return this._amount; 
+            }
+            private set 
+            {
+                this._amount=processValue(value);
+            }
+        }
+        public bool Is_Zero
+        {
+            get 
+            { 
+                return  _amount==0; 
+            }
+        }
+        //initilization
+        public decimal ConvertionFactor{ get; set; } =1.99m;
 
+        public void SetAmount(decimal value)
+        {
+            Amount=value; 
+        }
         public Doller(decimal amount)
         {
-            if(amount <= 0)
-            {
-
-                this._amount= 0;
-            }
-            _amount = amount;    
+            processValue(amount);
         }
+        private decimal processValue(decimal value) => value <= 0 ? 0 : Math.Round(value);
+        
     }
 }
