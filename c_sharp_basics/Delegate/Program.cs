e093dd540f895d9854844d5eb91f760623bac6ff
asemahmedghareeb <asemelbadahy123@gmail.com>
2022-10-14 18:16:31 +0200

    new

---
+++ c_sharp_basics/Delegate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Delegate
+{
+    class Delegate
+    {
+        static void Main()
+        {
+            var emps = new Employee[]
+            {
+                new Employee{Id=1,Name="Issam",Gender="m",TotalSales=234234 },
+                new Employee{Id=2,Name="Issam",Gender="m",TotalSales=245987765 },
+                new Employee{Id=3,Name="Issam",Gender="m",TotalSales=229034 },
+                new Employee{Id=4,Name="Issam",Gender="m",TotalSales=243904 },
+                new Employee{Id=5,Name="Issam",Gender="m",TotalSales=230434 },
+                new Employee{Id=6,Name="Issam",Gender="m",TotalSales=203424 },
+                new Employee{Id=7,Name="Issam",Gender="m",TotalSales=2773234 },
+                new Employee{Id=8,Name="Issam",Gender="m",TotalSales=234274 }
+
+            };
+        }
+    }
+}
---
+++ c_sharp_basics/Delegate/Report.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Delegate
+{
+    public  class Report
+    {
+        public void processEmployeeWith60000PlusSales(Employee[] employeeps)
+        {
+
+        }
+    }
+}
