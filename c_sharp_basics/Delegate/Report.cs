ef805c912f6d5f7003de8c3c3dd0f4ae1b4a937e
asemahmedghareeb <asemelbadahy123@gmail.com>
2022-10-18 23:30:49 +0200

    new

--- c_sharp_basics/Delegate/Report.cs
+++ c_sharp_basics/Delegate/Report.cs
@@ -4,9 +4,67 @@ namespace Delegate
 {
     public  class Report
     {
-        public void processEmployeeWith60000PlusSales(Employee[] employeeps)
+        public delegate bool illegibleSales(Employee e);
+        public void ProssesEmployee(Employee[] employees,string title, illegibleSales IsIllegible)
         {
+            Console.WriteLine(title);
+            Console.WriteLine("-----------------------------");
 
+            foreach (var E in employees)
+            {
+                if (IsIllegible(E))
+                {
+                    Console.WriteLine($"{E.Id} | {E.Name} | {E.Gender} | {E.TotalSales} ");
+                }
+            }
+            Console.WriteLine("\n\n");
         }
+
+        //public void processEmployeeWith60000PlusSales(Employee[] employees)
+        //{
+        //    Console.WriteLine("Employee with $60,000 sales");
+        //    Console.WriteLine("-----------------------------");
+
+        //    foreach(var E in employees)
+        //    {
+        //        if (E.TotalSales >= 60000)
+        //        {
+        //            Console.WriteLine($"{E.Id} | {E.Name} | {E.Gender} | {E.TotalSales} ");
+        //        }
+        //    }
+        //    Console.WriteLine("\n\n");
+        //}   
+        //public void processEmployeeWithSalesBetween30000And59000(Employee[] employees)
+        //{
+        //    Console.WriteLine("Employee with Sales between $30,000 and $59,000sales");
+        //    Console.WriteLine("-----------------------------");
+
+        //    foreach(var E in employees)
+        //    {
+        //        if (E.TotalSales >= 30000&&E.TotalSales<=60000)
+        //        {
+        //            Console.WriteLine($"{E.Id} | {E.Name} | {E.Gender} | {E.TotalSales} ");
+        //        }
+        //    }
+        //    Console.WriteLine("\n\n");
+        //}
+                
+        //public void processEmployeeWithSaleLessThan30000(Employee[] employees)
+        //{
+        //    Console.WriteLine("Employee with Sales Less than $30000");
+        //    Console.WriteLine("-----------------------------");
+
+        //    foreach(var E in employees)
+        //    {
+        //        if (E.TotalSales < 30000)
+        //        {
+        //            Console.WriteLine($"{E.Id} | {E.Name} | {E.Gender} | {E.TotalSales} ");
+        //        }
+        //    }
+        //    Console.WriteLine("\n\n");
+        //}
+
+
+
     }
 }
