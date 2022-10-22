d7d03d97ee2fcb99950fdbdf674f06587359eff9
asemahmedghareeb <asemelbadahy123@gmail.com>
2022-10-22 16:59:24 +0200

    new

---
+++ c_sharp_basics/Events/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace CAEvent
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            var stock = new Stock("amazon");
+            stock.Price = 100m;
+            Console.WriteLine($"stock before changing : ${stock.Price}");
+            stock.ChangeStockPriceBy(0.05m);
+            Console.WriteLine($"stock after changing ${stock.Price}");
+
+        }
+    }
+
+    public class Stock
+    {
+        private string name;
+        private decimal price;
+
+        public string Name=>this.name;
+        public decimal Price 
+        { 
+            get => this.price;
+            set => this.price=value;
+        }
+
+        public Stock(string stockname)
+        {
+            this.name = stockname;
+           
+        }
+
+        public void ChangeStockPriceBy(decimal percent)
+        {
+            this.Price+=Math.Round(this.Price*percent);
+
+        }
+    }
+}
