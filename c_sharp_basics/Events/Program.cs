d99bc8065660cf2ffd007ef5f0ffcd237661d1a3
asemahmedghareeb <asemelbadahy123@gmail.com>
2022-10-31 00:09:14 +0200

    new

--- c_sharp_basics/Events/Program.cs
+++ c_sharp_basics/Events/Program.cs
@@ -7,19 +7,40 @@ namespace CAEvent
         {
             var stock = new Stock("amazon");
             stock.Price = 100m;
-            Console.WriteLine($"stock before changing : ${stock.Price}");
+            stock.OnPriceChanged += Stock_OnPriceChanged;
+            //Console.WriteLine($"stock before changing : ${stock.Price}");
+            //stock.ChangeStockPriceBy(0.05m);
+            //Console.WriteLine($"stock after changing ${stock.Price}");
             stock.ChangeStockPriceBy(0.05m);
-            Console.WriteLine($"stock after changing ${stock.Price}");
+            stock.ChangeStockPriceBy(-0.02m);
+            stock.ChangeStockPriceBy(0.00m);
+        }
 
+        private static void Stock_OnPriceChanged(Stock stock, decimal oldprice)
+        {
+            if (stock.Price > oldprice)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else if (oldprice > stock.Price)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+                Console.WriteLine($"{stock.Name} price is {stock.Price}");
         }
     }
-    public delegate void StockPriceChangeHander();
+    public delegate void StockPriceChangeHander(Stock stock,decimal oldprice);
 
     public class Stock
     {
         private string name;
         private decimal price;
 
+        public event StockPriceChangeHander OnPriceChanged;
         public string Name => this.name;
         public decimal Price
         {
@@ -35,7 +56,13 @@ namespace CAEvent
 
         public void ChangeStockPriceBy(decimal percent)
         {
+            decimal Oldprice=this.price;
+
             this.Price += Math.Round(this.Price * percent);
+            if(OnPriceChanged != null)//make sure there is subscribers
+            {
+                OnPriceChanged(this, Oldprice);
+            }
 
         }
     }
